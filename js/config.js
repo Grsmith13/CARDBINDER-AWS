45c9729cee240f4c227051672cf4ee494279796c
Grsmith13 <129551911+Grsmith13@users.noreply.github.com>
2024-12-06 15:28:12 -0600

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
-        userPoolId: 'us-east-2_6gRh5Jyya', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '49miu5rus7lc9p08gmjald3lhf', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'us-east-2_vxGMhvqSc', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '4vpgtuq0ovt2p1df9ma32n6g09', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'us-east-2' // e.g. us-east-2
     },
     api: {
